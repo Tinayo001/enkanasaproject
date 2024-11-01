e8ff612846430b40453f6728456fb6a236ed5eca
Tinayo001 <keiyatinayo@gmail.com>
2024-11-01 09:59:02 +0300

    just atry of front end

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder= 'static')
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html")
+
+if __name__=="__main":
+    app.run(host='0.0.0.0', debug=True)
