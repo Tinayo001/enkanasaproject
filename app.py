5133ee1d63b56de744d4acc17ef2fdc2ea4118c6
Tinayo001 <keiyatinayo@gmail.com>
2024-11-01 15:29:09 +0300

    added to my project

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_folder= 'static')
 
+JOBS = [
+                {
+            'id': 1,
+            'title': 'Data Analyst',
+            'location': "Nairobi, Kenya",
+            'Salary': 'Ksh, 400000'
+            },
+        {
+            'id': 2,
+            'title': 'Data scientist',
+            'location': "MOmbasa, Kenya",
+            'Salary': 'Ksh, 600000'
+            },
+        {
+            'id': 3,
+            'title': 'Front-end engineer',
+            'location': "Remote",
+            'Salary': 'Ksh, 500000'
+            },
+        {
+            'id': 4,
+            'title': 'Backend Engineer',
+            'location': "San Fransisco, USA",
+            'Salary': '$5000'
+            },
+        
+        ]
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",
+                           jobs=JOBS,
+                           company_name='Tinayo Devops')
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
 
 if __name__=="__main":
     app.run(host='0.0.0.0', debug=True)
